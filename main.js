46dccdce0911cc7bf45ef4ed93568a5b3b38ee01
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-01-21 20:51:41 +0200

    calculator

---
+++ main.js
@@ -0,0 +1,7 @@
+let a = prompt ("Введите число")
+let b = prompt ("Введите второе число")
+let c = Number(a)+Number(b)
+let d = a-b
+let e = a*b
+let f = a/b
+alert(" Сложение:" + c + " Вычетание:" + d + " Умножение:" + e + " Деление:" + f) 
